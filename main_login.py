836d9d14815303000ff12dcf81f6b209b3a9953b
ashus190301 <88091881+ashus190301@users.noreply.github.com>
2022-10-09 13:18:29 +0530

    signup() function added

--- main_login.py
+++ main_login.py
@@ -38,3 +38,35 @@ def login():
                   con.close()
             except Exception as es:
                   messagebox.showerror("Error" , f"Error Dui to : {str(es)}", parent = win)
+def signup():
+      # signup database connect 
+      def action():
+            if first_name.get()=="" or last_name.get()=="" or user_name.get()=="" or password.get()=="" or very_pass.get()=="":
+                  messagebox.showerror("Error" , "All Fields Are Required" , parent = winsignup)
+            elif password.get() != very_pass.get():
+                  messagebox.showerror("Error" , "Password & Confirm Password Should Be Same" , parent = winsignup)
+            else:
+                  try:
+                        con = pymysql.connect(host="localhost",user="root",password="",database="criminaldb")
+                        cur = con.cursor()
+                        cur.execute("select * from user_information where username=%s",user_name.get())
+                        row = cur.fetchone()
+                        if row!=None:
+                              messagebox.showerror("Error" , "User Name Already Exits", parent = winsignup)
+                        else:
+                              cur.execute("insert into user_information(first_name,last_name,gender,username,password) values(%s,%s,%s,%s,%s)",
+                                    (
+                                    first_name.get(),
+                                    last_name.get(),
+                                    var.get(),
+                                    user_name.get(),
+                                    password.get()
+                                    ))
+                              con.commit()
+                              con.close()
+                              messagebox.showinfo("Success" , "Ragistration Successfull" , parent = winsignup)
+                              clear()
+                              switch()
+                        
+                  except Exception as es:
+                        messagebox.showerror("Error" , f"Error Dui to : {str(es)}", parent = winsignup)
