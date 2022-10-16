f1d9198f69301949878a96157005d72cd22f766b
avisharma344 <57820726+avisharma344@users.noreply.github.com>
2022-10-16 15:53:38 +0530

    Update register.py

--- register.py
+++ register.py
@@ -9,8 +9,12 @@ def registerCriminal(img, path, img_num):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = detect_faces(gray)
 
+
     if(len(faces) > 0):
         # Taking the largest face detected
         faces = sorted(faces, key=lambda x: x[3], reverse=True)  # sort based on height of image
         face_i = faces[0]
         (x, y, w, h) = [v * size for v in face_i]
+
+    face = gray[y:y + h, x:x + w]
+        face = cv2.resize(face, (im_width, im_height))
