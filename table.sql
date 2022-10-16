632c850d39f33c9fec966ff56eebfcb76a687a50
NehaVedak <92975018+NehaVedak@users.noreply.github.com>
2022-10-16 16:56:41 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -17,3 +17,17 @@ CREATE TABLE `criminaldata` (
 --
 -- Dumping data for table `criminaldata` as it is
 --
+  
+git commit -m "Criminal Insertion Feature added"
+  INSERT INTO `criminaldata` (`Criminal-ID`, `Address`, `Phone`, `Name`, `Father's Name`, `Gender`, `DOB(yyyy-mm-dd)`, `Crimes Done`, `Date of Arrest`, `Place of Arrest`) VALUES
+(123456, 'jaipur', 0, 'vivek rati', 'mahesh rathi', 'male', '2000-09-07', 'fraud', '2020-12-09', 'chhattisgarh'),
+(234, 'jabalpur', 0, 'jaydeep singh', 'mahendra singh', 'male', '1970-09-08', 'fraud', '2009-08-07', 'jabalpur'),
+(567432, 'delhi', 0, 'pranav verma', 'ramesh verma', 'male', '2000-05-04', 'murder', '2000-05-04', 'aagra'),
+(876594, 'jharkhand', 0, 'mehul sharma', 'mahesh sharma', 'male', '1999-09-09', 'fraud', '1999-09-09', 'odisha'),
+(1234, 'dfg', 0, 'bjk', 'mn', 'm', '1990-09-08', 'b,j', '2000-09-08', 'hv'),
+(123456, 'h', 0, 'c', 'c', 'c', '1990-08-09', 'd', '1990-08-09', 'd'),
+(89, 'gh', 0, 'nmk', ' h', 'm', '1998-09-08', 'vb', '1998-09-08', 'cgj'),
+(409087, 'delhi', 0, 'rajesh yadav', 'brijesh yadav', 'male', '1978-09-08', 'fraud', '2019-08-09', 'mumbai'),
+(4, 'g', 0, 'v', 'v', 'v', '2020-09-09', 'm', '2020-09-09', 'g'),
+(98, 'v', 0, 'c', 'c', 'c', '2020-09-09', 'f', '2020-09-09', 'f'),
+(4, 'g', 47, 'cfc', 'fre', 'm', '2020-09-09', 'm', '2020-09-09', 'v');
